d8dd8e78ad82e073b44baf59fb8fb2e3dd8cd64e
jgkim <jgkim@cybermed.co.kr>
2019-01-08 09:55:23 +0900

    2-3

--- src/main/java/com/example/webservice/web/WebRestController.java
+++ src/main/java/com/example/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.example.webservice.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.webservice.domain.PostsRepository;
+import com.example.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
