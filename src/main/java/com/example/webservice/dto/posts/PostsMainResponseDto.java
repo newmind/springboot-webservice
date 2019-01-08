1d288b344fc6c0b5771cf8b1b113eebdb1e11a0f
jgkim <jgkim@cybermed.co.kr>
2019-01-08 16:07:36 +0900

    3-2 end   글목록 보기

---
+++ src/main/java/com/example/webservice/dto/posts/PostsMainResponseDto.java
@@ -0,0 +1,44 @@
+package com.example.webservice.dto.posts;
+
+import com.example.webservice.domain.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Getter
+public class PostsMainResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String modifiedDate;
+
+    public PostsMainResponseDto(Posts entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        author = entity.getAuthor();
+        modifiedDate = toStringDateTime(entity.getModifiedDate());
+    }
+
+    /**
+     * Java 8
+     * @param localDateTime
+     * @return
+     */
+    private String toStringDateTime(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return Optional.ofNullable(localDateTime)
+                .map(formatter::format)
+                .orElse("");
+    }
+
+    private String toStringDateTimeByJava7(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return "";
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(localDateTime);
+    }
+}
